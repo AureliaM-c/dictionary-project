0cd308a464e23253eb53031608d2403e9697ab1c
Aurelia <m_aurelia@hotmail.fr>
2026-02-03 13:53:20 +0000

    dictionary api integration + axios integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+
+    let apiKey = "dd09ob23ada50e374e3e4a89b5c9fte0";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
