d68f19234e476dc1c1d13216d65140c263ba581d
Aurelia <m_aurelia@hotmail.fr>
2026-02-03 13:03:41 +0000

    search engine initial set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./Logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />{" "}
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />{" "}
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Aurelia Mourey</footer>
+      </div>
     </div>
   );
 }
